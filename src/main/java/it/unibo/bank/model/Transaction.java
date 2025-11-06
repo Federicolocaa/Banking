92e262202de13d991c78a48acf1855531c6d7afe
Federico Locatelli <federicolocatelli@MacBook-Air-di-Federico.local>
2025-11-06 13:34:31 +0100

    Completato il punto 1.2, realizzata la classe Transiction

---
+++ src/main/java/it/unibo/bank/model/Transaction.java
@@ -0,0 +1,18 @@
+public class Transaction {
+    
+    private final String description;
+    private final double amount;
+
+    public Transaction(final String description, final double amount){
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public double getAmount(){
+        return this.amount;
+    }
+}
